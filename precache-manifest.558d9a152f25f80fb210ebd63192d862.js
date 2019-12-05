387cdf9d6deb9bb6e4f5f0800124052115e7bcf1
Alex Kafer <me@alexkafer.com>
2019-12-04 20:21:10 -0600

    Updates

---
+++ precache-manifest.558d9a152f25f80fb210ebd63192d862.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b649fe8481bf482718b538d606e5ca1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90ecc3c341d8cd0fc544",
+    "url": "/static/css/main.7f8049a5.chunk.css"
+  },
+  {
+    "revision": "940f3bf908884610e89c",
+    "url": "/static/js/0.ea1c28eb.chunk.js"
+  },
+  {
+    "revision": "c3974edf51289d61c361",
+    "url": "/static/js/1.e1444c9d.chunk.js"
+  },
+  {
+    "revision": "46a32749b258cd868855",
+    "url": "/static/js/2.2a8e2e21.chunk.js"
+  },
+  {
+    "revision": "520ce240d11edcd19aa2",
+    "url": "/static/js/3.77c62d01.chunk.js"
+  },
+  {
+    "revision": "b44d8cd3b2ddedc31fc2",
+    "url": "/static/js/4.bac64f65.chunk.js"
+  },
+  {
+    "revision": "8bf7105015020e248934",
+    "url": "/static/js/7.5ca92729.chunk.js"
+  },
+  {
+    "revision": "90ecc3c341d8cd0fc544",
+    "url": "/static/js/main.9ee31e1e.chunk.js"
+  },
+  {
+    "revision": "a3c299167c950d56b6bc",
+    "url": "/static/js/runtime-main.39d0dfb7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e26d98c8bd0d8e173f4facfcbf745dee.js"
+  "/precache-manifest.558d9a152f25f80fb210ebd63192d862.js"
 );
 
 self.addEventListener('message', (event) => {
